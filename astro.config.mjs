52ad4469c72d21b9840e5def5c0d23bc6a4d4559
Sam Young <sam.young@infoworks-tn.com>
2026-05-27 10:52:07 -0500

    changes to styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sugarcube from "@sugarcube-sh/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [sugarcube()],
+  },
+});
